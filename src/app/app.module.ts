c6a05155f62e933c6bfed7682fa5b08e3046db7e
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-03-03 23:48:17 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
